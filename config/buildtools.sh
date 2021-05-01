44c93fa6dd4c0958b6564649a437b1b618497fae
mozl3 <82871514+mozl3@users.noreply.github.com>
2021-05-01 15:06:19 +0200

    Busybox flag

--- config/buildtools.sh
+++ config/buildtools.sh
@@ -3,6 +3,7 @@ TOOLCHAIN="https://toolchains.bootlin.com/downloads/releases/toolchains/armv6-ea
 TOOLCHAIN_PATH=$PWD/toolchain
 PULL_TOOLCHAIN=true
 PULL_LINUX=true
+PULL_BUSYBOX=true
 #If you set this to false you'll have to provide your own rootfs.cpio.gz for now
 PULL_BUILDROOT=true
 MAKE_FLAGS=-j$(nproc)
